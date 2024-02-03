74ce35a41d065c3fe8a36cc8932c11c53c2e4325
Yehor Kashperskyi <yehor.kashperskyi@viveapp.com>
2024-02-04 01:28:39 +0200

    feat: Implement colour scheme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { AfterViewInit, Component, Inject, Renderer2 } from '@angular/core';
 
 import { NumberModel, SliderModel } from 'look-n-play';
 
@@ -9,12 +10,22 @@ import { NumberModel, SliderModel } from 'look-n-play';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   public slider: SliderModel;
   public number: NumberModel;
 
-  public constructor() {
+  public constructor(
+    private _renderer2: Renderer2,
+    @Inject(DOCUMENT) private _document: Document,
+  ) {
     this.slider = new SliderModel(0, 100, 47.7);
     this.number = new NumberModel(201.7, 10, true);
+  }
+
+  public ngAfterViewInit(): void {
+    /**
+     * Link-up colour-scheme to document
+    **/
+    this._renderer2.addClass(this._document.body, 'theme-light');
   }
 }
